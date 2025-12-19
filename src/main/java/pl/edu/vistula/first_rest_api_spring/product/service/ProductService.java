a23d2c176ee1757e8e56824578b519d8b7850dd5
Dudley Mashava <seandmashava@gmail.com>
2025-12-19 14:57:20 +0100

    added to ProductService.java

--- src/main/java/pl/edu/vistula/first_rest_api_spring/product/service/ProductService.java
+++ src/main/java/pl/edu/vistula/first_rest_api_spring/product/service/ProductService.java
@@ -1,4 +1,24 @@
 package pl.edu.vistula.first_rest_api_spring.product.service;
 
+import org.springframework.stereotype.Service;
+import pl.edu.vistula.first_rest_api_spring.product.api.request.ProductRequest;
+import pl.edu.vistula.first_rest_api_spring.product.api.response.ProductResponse;
+import pl.edu.vistula.first_rest_api_spring.product.domain.Product;
+import pl.edu.vistula.first_rest_api_spring.product.repository.ProductRepository;
+import pl.edu.vistula.first_rest_api_spring.product.support.ProductMapper;
+
+@Service
 public class ProductService {
+    private ProductRepository productRepository;
+    private ProductMapper productMapper;
+
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    public ProductResponse create(ProductRequest productRequest) {
+        Product product = productRepository.save(productMapper.toProduct(productRequest));
+        return productMapper.toProductResponse(product);
+    }
 }

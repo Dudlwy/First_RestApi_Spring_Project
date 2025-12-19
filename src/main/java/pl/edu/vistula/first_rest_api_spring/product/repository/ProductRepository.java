cb6833b1e81c1fa7f37e7894424ee5cdd0b42e91
Dudley Mashava <seandmashava@gmail.com>
2025-12-19 14:24:42 +0100

    added code to ProductRepository.Java

--- src/main/java/pl/edu/vistula/first_rest_api_spring/product/repository/ProductRepository.java
+++ src/main/java/pl/edu/vistula/first_rest_api_spring/product/repository/ProductRepository.java
@@ -1,4 +1,29 @@
 package pl.edu.vistula.first_rest_api_spring.product.repository;
 
+import org.springframework.stereotype.Repository;
+import pl.edu.vistula.first_rest_api_spring.product.domain.Product;
+import pl.edu.vistula.first_rest_api_spring.product.support.ProductMapper;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class ProductRepository {
+
+    protected final Map<Long, Product> map = new HashMap<>();
+    protected long counter = 1;
+
+    public Product save(Product entity){
+        setID(entity);
+        return entity;
+    }
+
+    private Product setID(Product entity){
+        entity.setId(counter);
+        map.put(counter, entity);
+        counter++;
+        return entity;
+    }
+
 }

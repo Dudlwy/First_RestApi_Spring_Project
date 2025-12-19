11548758adb7f622c5ffa00c87bdacfffb28c0b6
Dudley Mashava <seandmashava@gmail.com>
2025-12-19 14:02:55 +0100

    added to Product.java

--- src/main/java/pl/edu/vistula/first_rest_api_spring/product/domain/Product.java
+++ src/main/java/pl/edu/vistula/first_rest_api_spring/product/domain/Product.java
@@ -1,4 +1,26 @@
 package pl.edu.vistula.first_rest_api_spring.product.domain;
 
 public class Product {
+    private long id;
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
